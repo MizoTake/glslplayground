ee97b3af49e45497df7ce55d8b5d2d6175830a3c
MizoTake <mizotake123@gmail.com>
2020-05-03 20:17:44 +0900

    wip

--- playground.glsl
+++ playground.glsl
@@ -1,34 +1,16 @@
 float TIME;
 
-vec2 circle(vec2 p) {
-    return vec2(length(p), 0.7);
-}
-
-vec2 square(vec2 p) {
-    return vec2(abs(p.x) + abs(p.y), 0.5);
-}
-
-vec2 white(vec2 p) {
-    return vec2(0, 1);	
-}
-
 vec2 rotation(vec2 v, float a) {
-    float s = sin(a);
-    float c = cos(a);
-    mat2 m = mat2(c, -s, s, c);
-    return m * v;
+    return mat2(cos(a), -sin(a), sin(a), cos(a)) * v;
 }
 
 vec2 morphing(vec2 p) {
 	float t = TIME * 2.5;
-	
 	int pair = int(floor(mod(t, 3.0)));
-	
 	float a = smoothstep(0.2, 0.8, mod(t, 1.0));
-	
-	if (pair == 0) return mix(square(p), circle(p), a);
-	if (pair == 1) return mix(circle(p), white(p), a);
-	else return mix(white(p), square(p), a);
+	if (pair == 0) return mix(vec2(abs(p.x) + abs(p.y), 0.5), vec2(length(p), 0.7), a);
+	if (pair == 1) return mix(vec2(length(p), 0.7), vec2(0, 1), a);
+	else return mix(vec2(0, 1), vec2(abs(p.x) + abs(p.y), 0.5), a);
 }
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
@@ -43,3 +25,12 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 
     fragColor = vec4(color, 1.0);
 }
+
+// twigl 共有
+// void main(){
+//   vec2 p=(gl_FragCoord.xy*2.-r)/r.y,o=vec2(abs(p.x)+abs(p.y),.5),i=vec2(0,1);
+//   float a=smoothstep(.2,.8,mod(t,1.)),s=floor(mod(t,3.));
+//   vec2 d=mix(mix(mix(o,vec2(length(p),.7),a),mix(vec2(length(p),.9),i,a),s),mix(i,o,a),step(2.,s));
+//   p.xy=abs(p)/abs(dot(p,p))-vec2(cos(t)*.4);
+//   gl_FragColor=mix(p.yxx,p.yyx,step(d.y, d.x)).xyyy;
+// }
